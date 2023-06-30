52d1e8054f8bed62f58274b8a6f56ffe5bb004a0
jyjames <136003877+jyjames@users.noreply.github.com>
2023-06-30 22:35:47 +0800

    Add files via upload

--- back-end/index.js
+++ back-end/index.js
@@ -25,6 +25,7 @@ app.get("/", (req, res) => {
     res.json("Hello, this is the backend!")
 })
 
+//Query for ADMIN LOGIN
 app.post("/admin", (req, res) => {
     const { username, password } = req.body;
     // Check the username and password against the database
@@ -44,9 +45,22 @@ app.post("/admin", (req, res) => {
     });
 });
 
-  
+//Query for INSERTING ORDERS
 app.post("/orders", (req, res) => {
-    const q = "INSERT INTO orderdetails (orderDate, name, address, contact, orderMethod, paymentMethod, productID, productName, quantity, price) VALUES ?"
+    const q = 
+    `INSERT INTO 
+        orderdetails 
+            (orderDate, 
+            name, 
+            address,
+            contact,
+            orderMethod,
+            paymentMethod,
+            productID,
+            productName,
+            quantity,
+            price)
+        VALUES ?`
     const bulkValues = req.body.map((values) => [
         values.orderDate,
         values.name,
@@ -67,82 +81,196 @@ app.post("/orders", (req, res) => {
     
 })
 
+//Query for INCOME FOR THE DAY
 app.get("/dailyincome", (req, res) => {
-    const q = "SELECT SUM(price) FROM orderdetails WHERE orderDate = CURRENT_DATE()"
+    const q = 
+    `SELECT 
+        SUM(price) 
+    FROM 
+        orderdetails 
+    WHERE 
+        orderDate = CURRENT_DATE()`
     db.query(q, (err, data) => {
         if (err) return res.json(err)
         return res.send(data)
     })
 })
 
+//Query for INCOME FOR THE WEEK
 app.get("/weeklyincome", (req, res) => {
-    const q = "SELECT SUM(price) FROM orderdetails WHERE orderDate >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY);"
+    const q = 
+    `SELECT 
+        SUM(price) 
+    FROM 
+        orderdetails 
+    WHERE 
+        orderDate >= DATE_SUB(CURRENT_DATE(), INTERVAL 6 DAY);`
     db.query(q, (err, data) => {
         if (err) return res.json(err)
         return res.send(data)
     })
 })
 
+//Query for COUNTING ORDER FOR THE DAY
 app.get("/dailycount", (req, res) => {
-    const q = "SELECT SUM(quantity) FROM orderdetails WHERE orderDate = CURRENT_DATE();"
+    const q = 
+    `SELECT 
+        SUM(quantity) 
+    FROM 
+        orderdetails 
+    WHERE 
+        orderDate = CURRENT_DATE();`
     db.query(q, (err, data) => {
         if (err) return res.json(err)
         return res.send(data)
     })
 })
 
-app.get("/avgDailyIncome", (req, res) => {
-    const q = `SELECT AVG(daily_price) FROM ( SELECT DATE(orderDate) AS order_date, 
-    SUM(price) AS daily_price FROM orderdetails WHERE orderDate >= DATE_SUB(CURDATE(), 
-    INTERVAL 7 DAY) GROUP BY order_date ) AS daily_price_subquery;`
+//Query for AVERAGE WEEK INCOME
+app.get("/avgWeeklyIncome", (req, res) => {
+    const q = 
+    `SELECT 
+        AVG(daily_price) 
+    FROM 
+        (SELECT 
+            DATE(orderDate) AS order_date, 
+            SUM(price) AS daily_price 
+        FROM 
+            orderdetails 
+        WHERE 
+            orderDate >= DATE_SUB(CURDATE(), INTERVAL 6 DAY)
+        GROUP 
+            BY order_date)
+    AS 
+        daily_price_subquery;`
     db.query(q, (err, data) => {
         if (err) return res.json(err)
         return res.send(data)
     })
 })
 
+//Query for AVERAGE NO. OF ORDERS FOR THE WEEK
 app.get("/avgCountOrders", (req, res) => {
-    const q = `SELECT AVG(daily_order_count) FROM ( SELECT DATE(orderDate) AS order_date, 
-    SUM(quantity) AS daily_order_count FROM orderdetails WHERE orderDate >= DATE_SUB(CURDATE(), 
-    INTERVAL 7 DAY) GROUP BY order_date ) AS daily_orders_subquery;`
+    const q = 
+    `SELECT 
+        AVG(daily_order_count) 
+    FROM 
+        (SELECT 
+            DATE(orderDate) 
+        AS 
+            order_date, 
+            SUM(quantity) AS daily_order_count 
+        FROM 
+            orderdetails
+        WHERE
+            orderDate >= DATE_SUB(CURDATE(), INTERVAL 6 DAY)
+        GROUP BY
+            order_date) 
+    AS
+        daily_orders_subquery;`
     db.query(q, (err, data) => {
         if (err) return res.json(err)
         return res.send(data)
     })
 })
 
+//Query for LINE GRAPH - WEEK INCOME
 app.get("/groupIncome", (req, res) => {
-    const q = "SELECT orderDate, SUM(price) FROM orderdetails WHERE orderDate >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY) GROUP BY orderDate ORDER BY orderDate ASC;"
+    const q = 
+    `SELECT 
+        orderDate,
+        SUM(price)
+    FROM 
+        orderdetails 
+    WHERE 
+        orderDate >= DATE_SUB(CURRENT_DATE(), INTERVAL 6 DAY)
+    GROUP BY 
+        orderDate 
+    ORDER BY
+        orderDate ASC;`
     db.query(q, (err, data) => {
         if (err) return res.json(err)
         return res.send(data)
     })
 })
 
+//Query for BAR GRAPH - ORDER COUNTS
 app.get("/groupCount", (req, res) => {
-    const q = "SELECT orderDate, SUM(quantity) FROM orderdetails WHERE orderDate >= DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY) GROUP BY orderDate ORDER BY orderDate ASC;"
+    const q = 
+    `SELECT 
+        orderDate, 
+        SUM(quantity)
+    FROM
+        orderdetails 
+    WHERE
+        orderDate >= DATE_SUB(CURRENT_DATE(), INTERVAL 6 DAY)
+    GROUP BY
+        orderDate 
+    ORDER BY
+        orderDate ASC;`
     db.query(q, (err, data) => {
         if (err) return res.json(err)
         return res.send(data)
     })
 })
 
+//Query for BEST SELLING PRODUCT
 app.get("/bestSelling", (req, res) => {
-    const q = "SELECT productName FROM happy_shawarma.orderdetails GROUP BY productName ORDER BY COUNT(productID) DESC LIMIT 1;"
+    const q = 
+    `SELECT 
+        productName
+    FROM
+        orderdetails
+    GROUP BY
+        productName
+    ORDER BY
+        COUNT(productID) DESC 
+    LIMIT 1;`
     db.query(q, (err, data) => {
         if (err) return res.json(err)
         return res.send(data)
     })
 })
 
+//Query for DISPLAYING ORDERS
 app.get("/displayOrders", (req, res) => {
-    const q = "SELECT name, address, contact, orderMethod, paymentMethod, orders FROM (SELECT name, address, contact, orderMethod, paymentMethod, GROUP_CONCAT(CONCAT(quantity, ' x ', productName) SEPARATOR ', ') AS orders, MAX(orderID) AS maxOrderID FROM orderdetails WHERE DATE(orderDate) = CURDATE() GROUP BY name, address, contact, orderMethod, paymentMethod) AS subquery ORDER BY maxOrderID;"
+    const q = 
+    `SELECT 
+        name,
+        address,
+        contact,
+        orderMethod,
+        paymentMethod,
+        orders 
+    FROM 
+        (SELECT 
+            name,
+            address,
+            contact,
+            orderMethod,
+            paymentMethod,
+        GROUP_CONCAT(CONCAT(quantity, ' x ', productName) SEPARATOR ', ') AS orders,
+        MAX(orderID) AS maxOrderID
+        FROM 
+            orderdetails
+        WHERE
+            DATE(orderDate) = CURDATE()
+        GROUP BY
+            name,
+            address,
+            contact,
+            orderMethod,
+            paymentMethod)
+        AS subquery
+    ORDER BY
+        maxOrderID;`
     db.query(q, (err, data) => {
         if (err) return res.json(err)
         return res.send(data)
     })
 })
 
+//Query for PIE CHART - 5 BEST SELLING PRODUCTS
 app.get("/groupBestSelling", (req, res) => {
     const q = `
     SELECT
